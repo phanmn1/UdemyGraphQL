e434916fd588caa04b8f9018304183f82692e65f
Minh Phan <mphan@Minhs-MacBook-Pro.local>
2020-08-22 16:08:21 -0700

    Section 6.60

--- src/Resolvers/Query.js
+++ src/Resolvers/Query.js
@@ -1,23 +1,51 @@
 const Query = {
-    users(parent, args, {db}, info) {
-        if(!args.query) {
-            return db.users
-        }
-        
-        return db.users.filter((user) => {
-            return user.name.toLowerCase().includes(args.query.toLowerCase())
-        })
-    },
-    posts(parent, args, { db }, info) {
-        if(!args.query) {
-            return db.posts
+    users(parent, args, { prisma }, info) {
+
+        const opArgs = {}
+
+        if(args.query) {
+            opArgs.where = {
+                OR : [
+                {
+                    name_contains: args.query
+                }, {
+                    email_contains: args.query
+                }
+                ]
+            }
         }
 
-        return db.posts.filter(post => {
-            const isTitleMatch = post.title.toLocaleLowerCase().includes(args.query.toLowerCase())
-            const isBodyMatch = post.body.toLocaleLowerCase().includes(args.query.toLowerCase())
-            return isBodyMatch || isTitleMatch 
-        })
+        return prisma.query.users(opArgs, info)
+        // if(!args.query) {
+        //     return db.users
+        // }
+        
+        // return db.users.filter((user) => {
+        //     return user.name.toLowerCase().includes(args.query.toLowerCase())
+        // })
+    },
+    posts(parent, args, { prisma }, info) {
+        const opArgs = {}
+
+        if(args.query) {
+            opArgs.where = {
+                OR: [{
+                    title_contains: args.query
+                }, {
+                    body_contains: args.query
+                }]
+            }
+        }
+        return prisma.query.posts(opArgs, info)
+        // if(!args.query) {
+        //     return db.posts
+        // }
+
+        // return db.posts.filter(post => {
+        //     const isTitleMatch = post.title.toLocaleLowerCase().includes(args.query.toLowerCase())
+        //     const isBodyMatch = post.body.toLocaleLowerCase().includes(args.query.toLowerCase())
+        //     return isBodyMatch || isTitleMatch 
+        // })
     },
     me() {
         return {
